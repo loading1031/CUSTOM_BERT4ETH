5d4a5973cccb92647b3f860d8eed9a7ea2344da1
loading1031 <tjdans1031@gmail.com>
2025-08-11 14:20:16 +0900

    feat: cluster epoch50 피싱 결과

--- pretrain/run.sh
+++ pretrain/run.sh
@@ -16,14 +16,18 @@ python -m cluster.run_pretrain --bizdate=0000
 
 # 4. mlm/cluster 임베딩 최종
 # 커맨드 경로: pretrain/
-python -m rn_embed --bizdate=0000 --init_checkpoint=mlm/cpkt_local/epoch_50.pth
-python -m run_embed --bizdae=0000 --init_checkpoint=cluster/cpkt_local/epoch_50.pth
+python -m run_embed --bizdate=0000 --init_checkpoint=mlm/cpkt_local/epoch_50.pth
+python -m run_embed --bizdate=0000 --init_checkpoint=cluster/cpkt_local/epoch_50.pth
 
 # 5. mlm/cluster 최종 임베딩 피싱 시각화 결과 확인
-# 커맨드 경로: pretrain/eval/
+# 커맨드 경로: eval/
  python phish_visualize.py --data_dir=mlm_epoch_50
  python phish_visualize.py --data_dir=cluster_epoch_50
 
  # 6. 피싱 분류 성능 측정
- # 커맨드 경로: pretrain/eval/
+ # 커맨드 경로: eval/
  python phish_detection_mlp.py --input_dir=../outputs/mlm_epoch_50
+ python phish_detectionr_rfp.py --input_dir=../outputs/mlm_epoch_50
+
+ python phish_detection_mlp.py --input_dir=../outputs/cluster_epoch_50
+ python phish_detection_rf.py --input_dir=../outputs/cluster_epoch_50
